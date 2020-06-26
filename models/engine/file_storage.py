fd768906a79ed9539197469e371ecda850e7d285
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-25 20:38:13 -0500

    [Features]] New code for functionality of the class File Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,16 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 '''
     serializes instances to a JSON file and
     deserializes JSON file to instances
 '''
 import json
 
-class FileStorage: 
-	
+class FileStorage:
+
     def __init__(self, file_path, objects):
         ''' constructor '''
-        self.file_path
-        self.objects
+        self.file_path = "file.json"
+        self.objects = {}
 
 	@property
     def file_path(self):
@@ -42,13 +42,34 @@ class FileStorage:
             sets in __objects the obj with
             key <obj class name>.id
         '''
-		self.objects.update(obj.__class__.__name__)
+		self.objects[str(obj.__class__.__name__, + ',' + obj.id)] = obj
 
 	def save(self):
 		'''
-            save information of object in file JSON        
+            save information of object in file JSON
 		'''
 		with open(__file_path, mode='w', encoding='utf-8') as file:
 			file.write(json.dumps(__file_path))
 
-	def reload(self): 
+	def reload(self):
+        '''
+            load information of object in file JSON
+		'''
+        if __file_path:
+            with open(__file_path, mode='r', encoding='utf-8') as file:
+                self.objects = json.loads(__file_path)
+
+from models.base_model import BaseModel
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "Holberton"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
