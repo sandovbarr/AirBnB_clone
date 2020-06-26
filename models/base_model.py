805077352b24f176a2f8ac6396793c68f5039b0d
fernando <sandovbarr@gmail.com>
2020-06-26 18:50:50 -0500

    [ADD] newfeatures

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' defines all common attributes/methods for other classes '''
 import uuid
+import models
 from datetime import datetime
 
 
@@ -12,15 +13,14 @@ class BaseModel:
             if "id" in kwargs.keys():
                 self.id = kwargs["id"]
             if "created_at" in kwargs.keys():
-                self.created_at = datetime.\
-                    strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                self.created_at = datetime.now()
             if "updated_at" in kwargs.keys():
-                self.updated_at = datetime.\
-                    strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         ''' returns str representation '''
@@ -33,16 +33,16 @@ class BaseModel:
             updated_at with the current datetime
         '''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         '''
             returns a dictionary containing all
             keys/values of __dict__ of the instance
         '''
-        return_dict = {"__class__": __class__.__name__}
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        #  self.created_at.isoformat()
-        #  self.updated_at.isoformat()
-        return_dict.update(self.__dict__)
-        return (return_dict)
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        new_dict = {"__class__": __class__.__name__}
+        new_dict.update(self.__dict__)
+        new_dict['created_at'] = new_dict['created_at'].strftime(timeformat)
+        new_dict['updated_at'] = new_dict['updated_at'].strftime(timeformat)
+        return (new_dict)
