7e78453be6e5fd6e2121c83abb82c0293a0859e4
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-27 11:40:29 -0500

    [Add]: Init console

---
+++ console.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import cmd, sys
+
+class HBNBCommand(cmd.Cmd):
+
+	prompt = " (hbnb)"
+	def do_quit(self, args):
+		return True
+
+	def do_EOF(self, args):
+		return True
+
+	def emptyline(self):
+		pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
