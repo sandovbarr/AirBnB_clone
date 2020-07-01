bf02bd6d3ef06f5c3a350fe06d0ab198cbb585be
fernando <sandovbarr@gmail.com>
2020-06-30 19:55:01 -0500

    [ADD] previous test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,47 +1,73 @@
 #!/usr/bin/python3
-"""Module Test Case for FileStorage"""
-import unittest
-from datetime import datetime
-import re
+''' file storage unittesting '''
+
 import os
+import unittest
 import pep8
-import models
+import json
+from models.engine import file_storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from datetime import datetime
+import models
 
 
 class TestFileStorage(unittest.TestCase):
-    """FileStorage Test Class"""
-    @classmethod
-    def setUpClass(cls):
-        cls.p1 = Place()
-        cls.p1.city_id = "Richmond"
-        cls.p1.state_id = "VA"
-        cls.p1.number_rooms = 8
-        cls.p1.description = "awesome"
+    ''' class for test methods in file storage class'''
 
-    @classmethod
-    def tearDownClass(cls):
-        del cls.p1
+    def test_pep8(self):
+        """ Test that test/models/engine/file_storage.py conforms to PEP8 """
+        pep8style = pep8.StyleGuide(quiet=True)
+        path = 'tests/test_models/test_engine/test_file_storage.py''
+        r = pep8style.check_files([path])
+        self.assertEqual(r.total_errors, 0, "Fix Your PEP8 Style")
 
-    def tearDown(self):
-        """TearDown for each method in TestFileStorage class"""
-        if os.path.exists('file.json'):
-            os.remove('file.json')
+    def test_pep8_base(self):
+        """ Test the test file xD """
+        pep8style = pep8.StyleGuide(quiet=True)
+        r = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(r.total_errors, 0, "Fix Your PEP8 Style")
 
-    def test_style_check(self):
-        """
-        Tests pep8 style
-        """
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_docstring(self):
+        """test if docstring"""
+        self.assertIsNotNone(FileStorage.__doc__)
+
+    def test_docmodule(self):
+        """ Tests module """
+        self.assertTrue(len(file_storage.__doc__) >= 1)
+
+
+class TestFunctions(unittest.TestCase):
+    ''' functions unittesting '''
+
+    def setUp(self):
+        """Instance of the class"""
+        self.inst = FileStorage()
+
+    def test_new_all_modules(self):
+        '''Test for methods save, all and new of current class'''
+        storage = FileStorage()
+        self.assertTrue(type(storage), FileStorage)
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model.save()
+        all_objs = storage.all()
+        self.assertIsInstance(all_objs, dict)
+        key = str(my_model.__class__.__name__) + '.' + str(my_model.id)
+        self.assertTrue(key in all_objs.keys())
+
+    def save_module(self):
+        """check save_to_file"""
+        exists = os.path.exists('file.json')
+        self.assertTrue(exists, True)
+
+    def test_reload_method(self):
+        '''Check method of read file JSON for create objects'''
+        storage = FileStorage()
+        storage.reload()
+        all_objs = storage.all()
+        self.assertTrue(len(all_objs.keys()) > 0)
 
     def test_fs_instance(self):
         """FileStorage class save checks, reload checks"""
@@ -63,7 +89,3 @@ class TestFileStorage(unittest.TestCase):
             models.save(b1)
             models.reload(b1)
             models.all(b1)
-
-
-if __name__ == '__main__':
-    unittest.main()
