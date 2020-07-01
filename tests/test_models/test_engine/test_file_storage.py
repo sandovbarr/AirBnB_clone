04acc65e820b8eb7e7b41e31da91253876aeae6f
fernando <sandovbarr@gmail.com>
2020-06-30 20:55:20 -0500

    [Updeate]: new case

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -66,30 +66,3 @@ class test_file_storage(unittest.TestCase):
         ''' test for new method (error)'''
         with self.assertRaises(TypeError):
             storage.new()
-
-    def test_new_classes(self):
-        ''' test for new method (valid) '''
-        obj = BaseModel(id='00')
-        obj_key = 'BaseModel' + '.' + obj.id
-        obj1 = User(id='01')
-        obj1_key = 'User' + '.' + obj1.id
-        obj2 = City(id='02')
-        obj2_key = 'City' + '.' + obj2.id
-        obj3 = Amenity(id='03')
-        obj3_key = 'Amenity' + '.' + obj3.id
-        obj4 = Place(id='04')
-        obj4_key = 'Place' + '.' + obj4.id
-        obj5 = Review(id='05')
-        obj5_key = 'Review' + '.' + obj5.id
-        obj6 = State(id='06')
-        obj6_key = 'State' + '.' + obj6.id
-
-        self.assertEqual(storage.all(), {})
-        storage.new(obj1)
-        self.assertEqual(obj, storage.all()[obj_key])
-        self.assertEqual(obj1, storage.all()[obj1_key])
-        self.assertEqual(obj2, storage.all()[obj2_key])
-        self.assertEqual(obj3, storage.all()[obj3_key])
-        self.assertEqual(obj4, storage.all()[obj4_key])
-        self.assertEqual(obj5, storage.all()[obj5_key])
-        self.assertEqual(obj6, storage.all()[obj6_key])
