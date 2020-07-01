635f92020dc5cde53a7e879ff58d00e848789963
fernando <sandovbarr@gmail.com>
2020-06-30 19:10:25 -0500

    [DEL] unnecesary imports

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,7 @@
 '''
     import all modules
 '''
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
 from models.engine.file_storage import FileStorage
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 storage = FileStorage()
 storage.reload()
