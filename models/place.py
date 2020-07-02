9e22a741f671725fc1fe8014ce8373246065deaa
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-01 21:47:28 -0500

    [Add]: new string

--- models/place.py
+++ models/place.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     ''' Class to assing name of the Amenity'''
     city_id = ''
-    user_id = ""
+    user_id = ''
     name = ""
     description = ''
     number_rooms = 0
