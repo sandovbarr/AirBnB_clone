623cf4b1abc3ff379deb8c1b34d549e62162c41a
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-01 22:02:54 -0500

    [fix] name dic

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -78,6 +78,6 @@ class TestUser(unittest.TestCase):
 
     def test_kwargs(self):
         """Test the class - BaseModel passing kwargs """
-        dictonary = {'id': '662a23b3-abc7-4f43-81dc-64c000000c00'}
-        user1 = state.State(**dictonary)
+        dictionary = {'id': '662a23b3-abc7-4f43-81dc-64c000000c00'}
+        user1 = state.State(**dictionary)
         self.assertTrue(issubclass(user1.__class__, BaseModel))
