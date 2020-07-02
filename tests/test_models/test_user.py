7ebc7b9382ea3181c2e6f9a19fb43b139eec253a
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-01 21:54:09 -0500

    [Add]: uppercase name

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,7 +10,7 @@ from models.base_model import BaseModel
 import os
 
 
-class TestUserdocu(unittest.TestCase):
+class TestUserDocu(unittest.TestCase):
     ''' test Base Documentation '''
 
     def test_pep8(self):
