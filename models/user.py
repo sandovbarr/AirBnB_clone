597b94b29abb5ff8235e08a53a82833432ef6cf4
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-01 21:42:07 -0500

    [fix] new chr

--- models/user.py
+++ models/user.py
@@ -6,6 +6,6 @@ from models.base_model import BaseModel
 class User(BaseModel):
     ''' Class User inherits from BaseModel '''
     email = ''
-    password = ""
+    password = ''
     first_name = ""
     last_name = ""
